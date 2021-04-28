043cc59d2b46dfe1a1b5743ab12cbf982d41b7e7
kevinmilet <kevin.milet@gmail.com>
2021-04-28 16:10:05 +0200

    ...

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -9,7 +9,7 @@
 				<div class="col-xs-12 col-sm-6 col-md-3">
 					<div class="widget subscribe no-box">
 						<h5 class="widget-title">LE MAUVAIS COIN<span></span></h5>
-						<p>Une pâle copie de "Le Bon Coin".... </p>
+						<p>Une honteuse copie de "Le Bon Coin".... </p>
 					</div>
 				</div>
 
