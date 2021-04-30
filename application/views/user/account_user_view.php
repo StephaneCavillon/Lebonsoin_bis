dad42260077d585a49e225410023bfe083dd6065
Kevin Milet <72751666+kevinmilet@users.noreply.github.com>
2021-04-30 10:07:39 +0200

    responsive vue mon compte

--- application/views/user/account_user_view.php
+++ application/views/user/account_user_view.php
@@ -6,12 +6,12 @@
 		<div class="col">
 			<div class="shadow p-1 div">
 				<div class="row p-2 m-2">
-					<div class="col-sm-12 col-lg-9">
+					<div class="col-12 col-md-9">
 						<h5 class="card-title"><?=$user[0]->pseudo ?? ''?></h5>
 						<p><?=$user[0]->mail ?? ''?></p>
 					</div>
-					<div class="col-sm-12 col-lg-3 text-center">
-						<button class="btn btn-success mt-3" type="button" data-toggle="collapse"
+					<div class="col-12 col-md-3 text-center">
+						<button class="btn btn-success my-3" type="button" data-toggle="collapse"
 							data-target="#collapse">Modifier mes informations</button>
 						<a href="<?=base_url('index.php/user_controller/delete')?>" type="submit" name="del-account" class="text-danger">Supprimer mon compte</a>
 					</div>
@@ -38,8 +38,8 @@
 								<input type="date" name="birthdate" class="form-control"
 									value="<?=$user[0]->birthdate?>">
 							</div>
-							<div class="row p-2 justify-content-center">
-								<div class="col mt-3">
+							<div class="row p-2">
+								<div class="col text-center mt-3">
 									<button type="submit" name="submit" class="btn btn-success">Sauvegarder</button>
 								</div>
 							</div>
@@ -62,15 +62,15 @@
 				<div class="container">
 					<div class="row">
 						<div class="col-12">
-							<ul>
+							<ul class="pl-3">
 
 								<li class="mb-4">
-									<div class="row">
-										<div class="col-3">
+									<div class="row text-center">
+										<div class="col-md-3 mr-auto pl-0">
 											<img class="img-fluid" src="<?=base_url('assets/upload/1.jpg')?>" alt=""
 												width="100%">
 										</div>
-										<div class="col-6">
+										<div class="col-md-6">
 											<div class="row">
 												<h3><a href="">Peugeot 206</a></h3>
 											</div>
@@ -80,44 +80,15 @@
 											<div class="row">
 												<p>Véhicules</p>
 											</div>
-											<div class="row">
+											<div class="row mb-3">
 												<a href="<?=base_url('index.php/article_controller/edit_article/')?>"
 													class="btn btn-success">Modifier</a>
 												<a href="<?=base_url('index.php/article_controller/edit_article/')?>"
 													class="btn btn-footer ml-2">Supprimer</a>
 											</div>
 										</div>
-										<div class="col-3">
+										<div class="col-md-3">
 											<p>Publié le 10 mar 2021 13:00</p>
-										</div>
-									</div>
-								</li>
-
-								<li>
-									<div class="row">
-										<div class="col-3">
-											<img class="img-fluid" src="<?=base_url('assets/upload/4.jpg')?>" alt=""
-												width="100%">
-										</div>
-										<div class="col-6">
-											<div class="row">
-												<h3><a href="">Appartement Duplex 4 pièces</a></h3>
-											</div>
-											<div class="row">
-												<p><strong>120000 €</strong></p>
-											</div>
-											<div class="row">
-												<p>Véhicules</p>
-											</div>
-											<div class="row">
-												<a href="<?=base_url('index.php/article_controller/edit_article/')?>"
-													class="btn btn-success">Modifier</a>
-												<a href="<?=base_url('index.php/article_controller/edit_article/')?>"
-													class="btn btn-footer ml-2">Supprimer</a>
-											</div>
-										</div>
-										<div class="col-3">
-											<p>Publié le 28 avr 2021 14:10</p>
 										</div>
 									</div>
 								</li>
