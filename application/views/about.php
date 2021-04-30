24c683ccebb8d189cf1f90e6127859e4e4ae2fe1
Kevin Milet <72751666+kevinmilet@users.noreply.github.com>
2021-04-30 10:39:41 +0200

    ajout page about + liens dans footer

---
+++ application/views/about.php
@@ -0,0 +1,120 @@
+<div class="container">
+	<h1 class="text-center mb-5">A propos de nous</h1>
+	<div class="mb-5">
+		<h2 class="text-center mb-3">La Mauvais Coin c'est quoi?</h2>
+		<p class="text-justify">Avec Le Mauvais Coin, trouvez la bonne affaire sur le site référent de petites annonces de particulier à
+			particuliers. Avec une dixaine de petites annonces, trouvez la bonne occasion dans nos catégories voiture,
+			immobilier, emploi, vacances, mode, maison, jeux vidéo, etc… Déposez une annonce gratuite en toute
+			simplicité pour vendre, rechercher, donner vos biens de seconde main ou promouvoir vos services.</p>
+	</div>
+	<div class="mb-5">
+		<h2 class="text-center mb-3">Conditions générales</h2>
+		<div class="text-justify">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+				dolore
+				magna aliqua. Lorem dolor sed viverra ipsum nunc aliquet. Felis donec et odio pellentesque. Vitae et leo
+				duis ut. Erat nam at lectus urna duis convallis convallis tellus id. Vulputate enim nulla aliquet
+				porttitor
+				lacus luctus accumsan. Et netus et malesuada fames ac turpis egestas maecenas. Egestas integer eget
+				aliquet
+				nibh praesent tristique magna sit. Platea dictumst quisque sagittis purus sit amet. Nulla posuere
+				sollicitudin aliquam ultrices sagittis orci a scelerisque. Euismod nisi porta lorem mollis aliquam ut
+				porttitor leo. Ac tortor vitae purus faucibus. Commodo elit at imperdiet dui accumsan sit amet nulla.
+			</p>
+
+			<p>Senectus et netus et malesuada fames. A cras semper auctor neque vitae tempus quam pellentesque. Sed
+				augue
+				lacus viverra vitae. Aliquet lectus proin nibh nisl. Orci a scelerisque purus semper eget duis at.
+				Malesuada
+				bibendum arcu vitae elementum curabitur vitae. Enim neque volutpat ac tincidunt. Mauris vitae ultricies
+				leo
+				integer. Faucibus nisl tincidunt eget nullam non. Eget lorem dolor sed viverra ipsum. Aliquam malesuada
+				bibendum arcu vitae elementum curabitur vitae nunc sed. Congue eu consequat ac felis donec et odio
+				pellentesque diam. Id diam vel quam elementum pulvinar. Sagittis id consectetur purus ut faucibus
+				pulvinar
+				elementum integer enim. Eget gravida cum sociis natoque penatibus et magnis dis. Egestas diam in arcu
+				cursus
+				euismod quis viverra nibh.</p>
+
+			<p>Sed odio morbi quis commodo. Viverra mauris in aliquam sem. Pretium aenean pharetra magna ac placerat
+				vestibulum lectus mauris. Nibh mauris cursus mattis molestie a iaculis at erat. Nunc sed augue lacus
+				viverra
+				vitae. Arcu risus quis varius quam quisque. Aliquam purus sit amet luctus venenatis lectus. Non tellus
+				orci
+				ac auctor. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Odio morbi quis commodo odio
+				aenean
+				sed adipiscing. Placerat vestibulum lectus mauris ultrices eros in cursus turpis massa. Habitant morbi
+				tristique senectus et. Cursus sit amet dictum sit amet. Amet nulla facilisi morbi tempus iaculis urna id
+				volutpat lacus.</p>
+
+			<p> dictum fusce ut placerat orci. Molestie at elementum eu facilisis sed odio morbi quis commodo. Aliquam
+				purus
+				sit amet luctus. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Aliquet risus feugiat in ante
+				metus dictum. Tortor at risus viverra adipiscing. Aliquam ut porttitor leo a diam sollicitudin tempor
+				id.
+				Sit amet porttitor eget dolor morbi non. Mauris pellentesque pulvinar pellentesque habitant. Convallis
+				aenean et tortor at risus viverra adipiscing at in. Tellus id interdum velit laoreet id donec. Nibh nisl
+				condimentum id venenatis. Non diam phasellus vestibulum lorem. Vestibulum mattis ullamcorper velit sed
+				ullamcorper morbi. Egestas erat imperdiet sed euismod nisi.</p>
+
+			<p>Felis eget nunc lobortis mattis aliquam faucibus purus in massa. Sapien nec sagittis aliquam malesuada.
+				Vel
+				turpis nunc eget lorem dolor sed viverra ipsum nunc. Volutpat blandit aliquam etiam erat velit
+				scelerisque
+				in. Adipiscing elit pellentesque habitant morbi tristique senectus et netus et. Enim eu turpis egestas
+				pretium aenean pharetra magna ac. Vitae auctor eu augue ut lectus arcu bibendum at. Scelerisque in
+				dictum
+				non consectetur a erat nam. Mauris a diam maecenas sed enim ut sem. Velit dignissim sodales ut eu sem
+				integer vitae. Proin sagittis nisl rhoncus mattis. Non quam lacus suspendisse faucibus interdum posuere.
+				Habitasse platea dictumst vestibulum rhoncus est pellentesque elit.</p>
+
+			<p>Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut placerat. Mi sit amet mauris commodo quis
+				imperdiet massa tincidunt. Viverra aliquet eget sit amet tellus cras adipiscing enim. Dictum sit amet
+				justo
+				donec enim. Ante in nibh mauris cursus mattis molestie a iaculis. Netus et malesuada fames ac turpis
+				egestas
+				sed. In aliquam sem fringilla ut morbi. Tincidunt ornare massa eget egestas purus viverra accumsan.
+				Porta
+				nibh venenatis cras sed felis eget. Etiam non quam lacus suspendisse. Odio pellentesque diam volutpat
+				commodo sed. Velit laoreet id donec ultrices tincidunt arcu non. Id diam maecenas ultricies mi. Diam
+				donec
+				adipiscing tristique risus nec feugiat in fermentum. Amet consectetur adipiscing elit duis.</p>
+
+			<p> est lorem ipsum dolor sit amet consectetur adipiscing elit. Mi quis hendrerit dolor magna. Dictum sit
+				amet
+				justo donec enim diam. Sit amet justo donec enim diam vulputate ut pharetra sit. Massa id neque aliquam
+				vestibulum morbi blandit cursus. Sed turpis tincidunt id aliquet risus feugiat in ante. Vestibulum
+				rhoncus
+				est pellentesque elit ullamcorper dignissim cras. Rhoncus mattis rhoncus urna neque viverra. Quam id leo
+				in
+				vitae turpis massa sed. Ac turpis egestas sed tempus urna. Quis viverra nibh cras pulvinar mattis nunc
+				sed.
+				Elit scelerisque mauris pellentesque pulvinar. Amet nulla facilisi morbi tempus iaculis urna.
+				Ullamcorper
+				sit amet risus nullam eget. Lorem ipsum dolor sit amet consectetur adipiscing. Arcu bibendum at varius
+				vel
+				pharetra vel turpis nunc eget. Tincidunt lobortis feugiat vivamus at augue eget. Sapien faucibus et
+				molestie
+				ac feugiat sed lectus. Diam quis enim lobortis scelerisque fermentum dui faucibus in ornare. Morbi
+				tristique
+				senectus et netus et.</p>
+
+			<p> aliquam id diam maecenas ultricies mi eget mauris pharetra. Dolor sit amet consectetur adipiscing elit
+				pellentesque habitant morbi. Cras semper auctor neque vitae tempus quam pellentesque nec nam. Tempor
+				commodo
+				ullamcorper a lacus vestibulum sed arcu non. Elit eget gravida cum sociis. In egestas erat imperdiet
+				sed.
+				Sed risus ultricies tristique nulla aliquet enim tortor. Vel elit scelerisque mauris pellentesque
+				pulvinar.
+				Et tortor consequat id porta nibh venenatis cras sed felis. Sollicitudin nibh sit amet commodo nulla
+				facilisi nullam. Consectetur purus ut faucibus pulvinar elementum integer enim neque. Dictum non
+				consectetur
+				a erat nam. Aliquam sem fringilla ut morbi. Maecenas pharetra convallis posuere morbi leo urna molestie
+				at.
+				Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Scelerisque mauris pellentesque pulvinar
+				pellentesque habitant morbi tristique senectus. Et pharetra pharetra massa massa ultricies mi. Risus
+				pretium
+				quam vulputate dignissim suspendisse in est. Aenean pharetra magna ac placerat.</p>
+		</div>
+	</div>
+</div>
